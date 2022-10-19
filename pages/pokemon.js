0253dba6c04729d153b1b7bccf62302fa1ec2cd0
PanosKontos <panos96el@gmail.com>
2022-10-19 22:47:12 +0300

    changes

--- pages/pokemon.js
+++ pages/pokemon.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
+// import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import axios from 'axios';
+// import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 
